6e79dfef733238278c4f82a3d1c64a64f4a4574d
duncan <oluochdancun19@gmail.com>
2023-01-13 11:44:34 +0300

    deleted reference to index template

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,11 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/users/',include('users.urls') ),
     path('api/products/', include('products.urls')),
